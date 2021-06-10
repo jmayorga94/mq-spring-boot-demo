4ecc585dbd848a9bc42df2501f976ff10b1df1aa
jmayorga94 <javi.mayorga1994@gmail.com>
2021-06-10 14:42:56 -0600

    Iniciando proyecto

---
+++ src/main/java/com/devsessions/mqspringbootdemo/MqSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.devsessions.mqspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqSpringBootDemoApplication.class, args);
+	}
+
+}
